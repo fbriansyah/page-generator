e1230cc9d99a1bfbf784653643cb7292e0bd11b6
Febriansyah <febrian@efner.id>
2022-04-11 09:26:32 +0700

    fix typo

--- src/components/Tables/Table.svelte
+++ src/components/Tables/Table.svelte
@@ -10,21 +10,21 @@
       <thead>
         <tr class="border-t border-gray-200">
           {#each headers as header, i ("cell-" + header.label + "-" + i)}
-            <th class="{i === 0 ? "": ""}">{header.label}</th>
+            <th class="{i === 0 ? "w-full": ""}">{header.label}</th>
           {/each}
           <!-- <th
             class="hidden md:table-cell"
             >Last updated</th
           > -->
           <th
-            class="pr-6 py-3 border-b border-gray-200 bg-gray-50 text-right text-xs font-medium text-gray-500 uppercase tracking-wider"
+            class="pr-6 py-3 border-b border-gray-200 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
           >
             Actions
           </th>
         </tr>
       </thead>
       <tbody class="bg-white divide-y divide-gray-100">
-        <tr class="space-x-1">
+        <tr class="">
           {#each headers as h,i ("cell-" + h.label + "-" + i)}
             <td class="cell {i === 0 ? "lg:pl-2": ""}">
               <SkeletonCell />
@@ -32,10 +32,13 @@
           {/each}
           
           <td
-            class="px-6 py-3 whitespace-nowrap text-left text-sm font-medium"
+            class="px-6 py-3 whitespace-nowrap space-x-2 text-center text-sm font-medium"
           >
             <a href="#efner" class="text-indigo-600 hover:text-indigo-900"
               >Edit</a
+            >
+            <a href="#efner" class="text-red-600 hover:text-red-900"
+              >Delete</a
             >
           </td>
         </tr>
@@ -50,8 +53,9 @@
   th {
     @apply px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs;
     @apply font-medium text-gray-500 uppercase tracking-wider;
+    min-width: 6rem;
   }
   .cell {
-    @pply px-6 py-3 whitespace-nowrap text-sm text-gray-500 text-right;
+    @apply px-6 py-3 whitespace-nowrap text-sm text-gray-500;
   }
 </style>
